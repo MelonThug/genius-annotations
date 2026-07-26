5a1f86efe19e677c8481d8156a5607f2e91a68d2
aamiryatali <157635419+MelonThug@users.noreply.github.com>
2026-07-26 14:59:03 -0400

    fix: Selected lyric scrolling out of view due to previous annotation boxes closing. Current lyric now scrolls into view

--- src/components/Lyrics.tsx
+++ src/components/Lyrics.tsx
@@ -1,33 +1,43 @@
 import styles from '../css/app.module.scss'
-import React, { useState } from "react"
+import React, { useState, useRef, useLayoutEffect } from "react"
 import { Annotation } from '../types/annotation';
 import { sanitizeHtml } from '../functions/parsingFunctions';
 
 export default function Lyrics({lyrics, annotations}: {lyrics: Map<number, string>|null, annotations: Map<string, Annotation>|null}){
     const [hoveredAnnotationId, setHoveredAnnotationId] = useState<number | null>(null);
     const [selectedAnnotation, setSelectedAnnotation] = useState<{annotationId: number, lyricIndex: number} | null>(null);
+    const selectedLyricRef = useRef<HTMLSpanElement | null>(null);
+
+    useLayoutEffect(() => {
+        if(selectedAnnotation && selectedLyricRef.current){
+            selectedLyricRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "nearest"
+            })
+        }
+    }, [selectedAnnotation])
+
     if(!lyrics || lyrics?.size === 0) return;
 
     return (
             <div className={styles.lyrics_container} onClick={() => setSelectedAnnotation(null)}>
                 {Array.from(lyrics).map(([lyricIndex, line]) => {
                     if(line === "\n") return <br></br>;
-
                     if(line === "") return " ";
 
                     const normalized = line.toLowerCase();
-                    const annotation = new Map<string, Annotation>(annotations).get(normalized);
+                    const annotation = annotations?.get(normalized);
                     const isAnnotated = !!annotation;
                     const isSelected = isAnnotated && selectedAnnotation?.annotationId === annotation.id && selectedAnnotation?.lyricIndex === lyricIndex;
-                    const className = isAnnotated ? isSelected ? styles.lyrics_text_selected : styles.lyrics_text_annotated : styles.lyrics_text;
+                    const className = isAnnotated ? 
+                                      isSelected ? styles.lyrics_text_selected : styles.lyrics_text_annotated 
+                                      : styles.lyrics_text;
 
                     return (
                         <>
                             <span
-                            className=
-                            {`
-                            ${className}
-                            `}
+                            ref={isSelected ? selectedLyricRef : null}
+                            className={`${className}`}
                             data-annotation-id={annotation?.id}
                             data-lyricIndex={lyricIndex} 
                             onMouseEnter={() => annotation && setHoveredAnnotationId(annotation.id)}
@@ -35,6 +45,7 @@ export default function Lyrics({lyrics, annotations}: {lyrics: Map<number, strin
                             onClick={(e) => {
                                     e.stopPropagation();
                                     const isAlreadySelected = selectedAnnotation?.annotationId === annotation?.id && selectedAnnotation?.lyricIndex === lyricIndex;
+                                    
                                     if(isAlreadySelected){
                                         setSelectedAnnotation(null)
                                     } else if(annotation){
